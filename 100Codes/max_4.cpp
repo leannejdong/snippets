b9b41b26079beac9493e1eb5d79ce5a9a85499ab
Leanne Dong <jdleanne@gmail.com>
2021-06-20 12:05:23 -0400

    correction

--- 100Codes/max_4.cpp
+++ 100Codes/max_4.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
 int max_of_four(int a, int b, int c, int d)
 {
     return max(a, max(b, max(c, d)));  
 }
+int max4(int a, int b, int c, int d)
+{
+    return std::max({a, b, c, d});
+}
 int main() {
     int a, b, c, d;
+    cout << "Enter a, b, c d: " << "\n";
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    printf("%d\n", ans);
+   // cout << "Enter a, b, c d: " << "\n";
+   // scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans1 = max_of_four(a, b, c, d);
+    printf("%d\n", ans1);
     
     return 0;
 }
