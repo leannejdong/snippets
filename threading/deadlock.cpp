31be1aa4b41e947e1a3f7cc15c64c31eee849ff1
Leanne J Dong <leanne@bitwyre.com>
2021-10-10 00:11:09 -0400

    added some ugly deadlock examples

---
+++ threading/deadlock.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <mutex>
+
+int main()
+{
+    std::mutex mut;
+    std::cout << "\n";
+    std::cout << "first lock call\n";
+    mut.lock();
+    std::cout << "second lock call\n";
+    mut.lock();
+    std::cout << "first lock call\n";
+}
---
+++ threading/dl_fix.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mutex1, mutex2;
+void ThreadA()
+{
+    // Creates deadlock
+    mutex1.lock();
+    std::cout <<  "Thread A\n" ;
+    mutex2.lock();
+    mutex2.unlock();
+    mutex1.unlock();
+}
+
+void ThreadB()
+{
+    // Creates deadlock
+    mutex1.lock();
+    std::cout <<  "Thread B\n" ;
+    mutex2.lock();
+    mutex1.unlock();
+    mutex2.unlock();
+}
+
+void ExecuteThreads()
+{
+    std::thread t1(ThreadA);
+    std::thread t2(ThreadB);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Finished\n";
+}
+
+int main()
+{
+    ExecuteThreads();
+    return 0;
+}
+
+
