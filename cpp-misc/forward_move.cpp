a7330f5dc6137bbcf6551406803c2982d8e27b8a
Leanne J Dong <jdleanne@gmail.com>
2021-10-31 02:12:35 -0400

    revise perfect forwarding

---
+++ cpp-misc/forward_move.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void overloaded( int const &arg ) { std::cout << "by lvalue\n"; }
+void overloaded( int && arg ) { std::cout << "by rvalue\n"; }
+ 
+template< typename t >
+/* "t &&" with "t" being template param is special, and  adjusts "t" to be
+   (for example) "int &" or non-ref "int" so std::forward knows what to do. */
+void forwarding( t && arg ) {
+    std::cout << "via std::forward: ";
+    overloaded( std::forward< t >( arg ) );
+    std::cout << "via std::move: ";
+    overloaded( std::move( arg ) ); // conceptually this would invalidate arg
+    std::cout << "by simple passing: ";
+    overloaded( arg );
+}
+ 
+int main() {
+    std::cout << "initial caller passes rvalue:\n";
+    forwarding( 5 );
+    std::cout << "initial caller passes lvalue:\n";
+    int x = 5;
+    forwarding( x );
+}
+
+/* std::move vs std::forward
+td::move takes an object and allows you to treat it as a temporary (an rvalue). 
+Although it isn’t a semantic requirement, typically a function accepting a reference to an rvalue will invalidate it. 
+When you see std::move, it indicates that the value of the object should not be used afterwards, but you can still assign a new value and continue using it.
+std::forward has a single use case: to cast a templated function parameter (inside the function) to the value category (lvalue or rvalue) the caller used to pass it. 
+This allows rvalue arguments to be passed on as rvalues, and lvalues to be passed on as lvalues, a scheme called “perfect forwarding.”
+*/
