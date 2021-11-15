68de236d116ce759c952ea0025666258c2009c0b
Leanne J Dong <jdleanne@gmail.com>
2021-11-15 17:41:58 -0500

    add Liu's model

---
+++ insel/src/UBLIU2.java
@@ -0,0 +1,28 @@
+package eu.insel.userblock;
+
+import de.vseit.network.Attribute;
+import de.vseit.network.schema.Icon;
+import de.vseit.network.schema.StringType;
+import eu.insel.block.Block;
+import eu.insel.block.BlockInfo;
+
+@Icon(path="icons/ubcpp.png")
+
+@BlockInfo(function="ub0018",
+   group  = 3,
+   inMin  = 0,
+   inMax  = 0,
+   inIni  = 0,
+   outMin = 0,
+   outMax = 0,
+   outIni = 0,
+   bpMin  = 0,
+   bpMax  = 0,
+   spMin  = 0,
+   spMax  = 0)
+
+public final class UBLIU2 extends Block <UBLIU2>
+{
+   public UBLIU2(){}
+}
+
